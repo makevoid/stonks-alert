ddae460c278c242b659f6c21854d87119a3d2f92
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-03-09 14:52:12 +0000

    update stonks

--- config/stonks.rb
+++ config/stonks.rb
@@ -1,18 +1,15 @@
 STONKS = {
   # outliers
-  TSLA:   [580, 690], # demo
+  TSLA:   [520, 630], # demo
   SPY:    [370, 410],
   # tech stocks
   AMZN:   [2800, 3200],
-  AAPL:   [105, 160],
   # others
-  PLTR:   [23, 30],  # on reddit/news previous week
-  GME:    [75, 170],   # spiking again
-  # # TODO: use polygon.io ?
+  PLTR:   [18, 29], # random one to test
 }
 
 CRYPTOS = {
-  BTC:  [46000, 53000],
+  BTC:  [51000, 59000],
   # crypto values are in btc (except BTC)
   ETH:  [0.024, 0.04],
   DOGE: [0.0000005, 0.0000015],
