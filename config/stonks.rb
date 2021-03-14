cfdf5d36cab601184efdde75a8d20521324f2390
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-03-14 09:45:42 +0000

    update stonks

--- config/stonks.rb
+++ config/stonks.rb
@@ -2,11 +2,10 @@ STONKS = {
   TSLA:   [610, 750],
   SPY:    [370, 410],
   AMZN:   [2800, 3200],
-  PLTR:   [18, 29],
 }
 
 CRYPTOS = {
-  BTC:  [53000, 60000],
+  BTC:  [58000, 68000],
   # crypto values are in btc (except for BTC)
   ETH:  [0.024, 0.04],
   DOGE: [0.0000005, 0.0000015],
